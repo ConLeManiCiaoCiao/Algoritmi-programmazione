d0b763b2a17b666558cdbed27b109e44d68f86a5
ConLeManiCiaoCiao <100641520+ConLeManiCiaoCiao@users.noreply.github.com>
2022-04-16 22:37:20 +0200

    Add files via upload

---
+++ Lab_6/Es_1/es_1.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+void input_array(int *, size_t);
+void print_array(int *, size_t);
+double calc_avg(int *, size_t);
+int find_max(int *, size_t);
+void print_pos(int *, size_t, int);
+
+
+int main(void)
+{
+    int *v = NULL;
+    size_t dim = 0;
+    int max;
+
+    puts("Inserisci la dimensione del vettore:");
+    scanf("%lu", &dim);
+
+    v = malloc(dim * sizeof(int));
+
+    if(v == NULL)
+    {
+        fprintf(stderr, "Errore nell'allocazione del vettore...\n");
+        return EXIT_FAILURE;
+    }
+
+    input_array(v, dim);
+    print_array(v, dim);
+
+    printf("La media dei valori e': %.2f\n",
+           calc_avg(v, dim));
+
+    max = find_max(v, dim);
+    printf("Il massimo e': %d\n\n", max);
+    print_pos(v, dim, max);
+
+    free(v);
+
+    return EXIT_SUCCESS;
+}
+
+void input_array(int * v, size_t dim)
+{
+    size_t i;
+
+    puts("Inserisci il vettore:");
+
+    for(i = 0; i < dim; ++i)
+    {
+        scanf("%d", &v[i]);
+    }
+}
+
+void print_array(int * v, size_t dim)
+{
+    size_t i;
+
+    puts("Il vettore inserito e':");
+
+    for(i = 0; i < dim; ++i)
+    {
+        printf("[%d]\n", v[i]);
+    }
+
+    puts("");
+}
+
+double calc_avg(int * v, size_t dim)
+{
+    size_t i;
+    double avg = 0;
+
+    for(i = 0; i < dim; ++i)
+    {
+        avg += v[i];
+    }
+
+    return avg / dim;
+}
+
+int find_max(int * v, size_t dim)
+{
+    size_t i;
+
+    int max = v[0];
+
+    for(i = 1; i < dim; ++i)
+    {
+        if(max < v[i])
+        {
+            max = v[i];
+        }
+    }
+
+    return max;
+}
+
+void print_pos(int *v, size_t dim, int value)
+{
+    size_t i;
+
+    printf("L'elemento %d compare nelle seguenti posizioni:\n",
+            value);
+
+    for(i = 0; i < dim; ++i)
+    {
+        if(v[i] == value)
+        {
+            printf("%lu\n", i + 1);
+        }
+    }
+}
+
+
+/* EOF */
---
+++ Lab_6/Es_1/es_1.md
@@ -0,0 +1,14 @@
+## Esercizio 1
+
+Si scriva un programma in C che definisca e manipoli un vettore composto di N elementi interi, con N inserita da tastiera; il programma deve:
+
+
+1. Allocare dinamicamente il vettore della dimensione corretta.
+2. Acquisire i valori da tastiera e memorizzarli all'interno del vettore.
+3. Stampare il contenuto del vettore al termine dell'acquisizione.
+4. Calcolare e stampare la media dei valori del vettore.
+5. Individuare e stampare a video il valore massimo e la sua posizione ordinale nel vettore.
+6. Liberare la memoria precedentemente allocata.
+
+\
+Approfondimento: considerare il caso in cui il valore massimo occorra più di una volta e stamparne tutte le relative posizioni.
